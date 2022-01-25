a432a78660c3a4d2beccf3be1d5d2bd65c932582
Laksh25ay <lakshay.gupta@unthinkable.co>
2022-01-25 14:31:27 +0530

    contians latest build

--- src/Components/EventCalendar.jsx
+++ src/Components/EventCalendar.jsx
@@ -103,7 +103,6 @@ export default class EventCalendar extends Component {
           <ViewsDirective>
             <ViewDirective
               option="Day"
-              // interval={3}
               startHour="03:00"
               endHour="18:00"
             ></ViewDirective>
